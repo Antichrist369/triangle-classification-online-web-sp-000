5c8eeb83ad5c0063778d04653694dc12ab46d1dd
Anthony Molina <antonio-molina@att.net>
2019-08-26 04:34:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,4 +5,10 @@ class Triangle
     @triangle_sides << side_2 
     @triangle_sides << side_3 
   end 
+  
+  def valid? 
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1] 
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2] 
+    sum_one_three = @triangle_sides[1] + @triangle_sides[2] 
+  end 
 end

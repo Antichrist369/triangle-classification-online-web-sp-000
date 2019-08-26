80a3de7288614fd79711483b1a98cb2349ba569a
Anthony Molina <antonio-molina@att.net>
2019-08-26 04:46:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,4 +17,21 @@ class Triangle
      return false 
    end 
  end 
+ 
+ def kind 
+   if valid? 
+     if @triangle_sides.uniq.length == 1 
+       return :equilateral 
+     elsif @triangle_sides.uniq.length == 2 
+     return :isosceles 
+   else 
+     return :scalene 
+   end 
+   else 
+   raise TriangleError 
+   end 
+  end 
 end
+
+class TriangleError < StandardError 
+end 
